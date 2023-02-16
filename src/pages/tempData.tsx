e9539ff5f49f25ef490a95a1375ac4709d098ab0
Nithin47TA <nithinta234@gmail.com>
2023-02-16 18:28:59 +0530

    feat: data for video and news

--- src/pages/tempData.tsx
+++ src/pages/tempData.tsx
@@ -1,5 +1,5 @@
 import dayjs from "dayjs";
-import { Event, GeneralNews } from "../models/General Awarness";
+import { Event, GeneralNews, GeneralVideo } from "../models/General Awarness";
 
 let events: Event[] = [
   {
@@ -35,6 +35,42 @@ for (let i = 0; i < 50; i++) {
   events.push(events[1]);
 }
 
-let news: GeneralNews[] = [];
+let news: GeneralNews[] = [
+  {
+    id: "n1",
+    title: "drug mafia",
+    timestamp: dayjs(
+      "12/10/2010 12:00:00",
+      "DD-MM-YYYY HH:mm:ss"
+    ).toISOString(),
+    visible: true,
+    news_type: "INTERNAL",
+    description: "drug mafia captured in mbcet",
+  },
+  {
+    id: "n2",
+    title: "Counsellor turns drug supplier for jail inmates, held",
+    timestamp: dayjs("Feb 16, 2023, 07:54", "MM DD, YYYY, HH:mm").toISOString(),
+    visible: true,
+    news_type: "EXTERNAL",
+    redirect_url:
+      "https://timesofindia.indiatimes.com/city/ludhiana/counsellor-turns-drug-supplier-for-jail-inmates-held/articleshow/97965139.cms?from=mdr",
+  },
+];
 
-export { events, news };
+let videos: GeneralVideo[] = [
+  {
+    id: "vd1",
+    title: "Benzo Dope and Tranq: The Next Wave of the Overdose Crisis",
+    author: "EXCISE",
+    visible: true,
+    timestamp: dayjs("May 17, 2022", "MM DD, YYYY").toISOString(),
+    description: `<span dir="auto" class="style-scope yt-formatted-string">More than 100,000 Americans died of a drug overdose in 2021, the worst year on record. The newest wave of the overdose crisis involves man-made chemicals, including animal tranquilizers, that are being combined with fentanyl to make street drugs that are deadlier and more addictive.
+  With access to an undisguised fentanyl dealer, Beyond Fentanyl looks at how drugs like “benzo dope” and “tranq” are ravaging North American communities and how U.S. policy affected the latest flood of synthetic street drugs.
+  Help keep VICE News’ fearless reporting free for millions by making a one time or ongoing contribution here. - </span>`,
+    platform: "YOUTUBE",
+    url: "https://www.youtube.com/watch?v=82QhIOgJy1c",
+  },
+];
+
+export { events, news, videos };
