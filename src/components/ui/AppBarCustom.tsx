c1592e37c0ecffb1847cfa4d0f7159e820a7038d
Nithin47TA <nithinta234@gmail.com>
2023-04-08 01:55:40 +0530

    feat:burger for nav bar

--- src/components/ui/AppBarCustom.tsx
+++ src/components/ui/AppBarCustom.tsx
@@ -5,12 +5,19 @@ import {
   Button,
   Box,
   useTheme,
+  IconButton,
+  Drawer,
 } from "@mui/material";
 import { Outlet, NavLink } from "react-router-dom";
 import ROUTES from "routes";
+import "./AppBar.css";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+import { useState } from "react";
 
 export default function AppBarCustom() {
   const theme = useTheme();
+  const [isOpen, setIsOpen] = useState(false);
 
   const activeLink: React.CSSProperties = {
     backgroundColor: "#f9f9f9",
@@ -29,6 +36,30 @@ export default function AppBarCustom() {
   const applyNavStyle = ({ isActive }: { isActive: boolean }) =>
     isActive ? activeLink : inActiveLink;
 
+  const getDeviceConfig = (width: number) => {
+    if (width < 320) {
+      return true;
+    } else if (width >= 320 && width < 720) {
+      return true;
+    } else if (width >= 720 && width < 1024) {
+      return false;
+    } else if (width >= 1024) {
+      return false;
+    } else {
+      return false;
+    }
+  };
+
+  const [width, setWidth] = useState<Boolean>(() =>
+    getDeviceConfig(window.innerWidth)
+  );
+
+  window.addEventListener("resize", () =>
+    setWidth(getDeviceConfig(window.innerWidth))
+  );
+
+  const handleClose = () => setIsOpen(false);
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar
@@ -44,18 +75,65 @@ export default function AppBarCustom() {
           >
             Schello
           </Typography>
-          <NavLink style={applyNavStyle} to={ROUTES.DEFAULT} end>
-            <Button color="inherit">Home</Button>
-          </NavLink>
-          <NavLink style={applyNavStyle} to={ROUTES.DRUG_FORM} end>
-            <Button color="inherit">Form</Button>
-          </NavLink>
-          <NavLink style={applyNavStyle} to={ROUTES.COMMUNITY} end>
-            <Button color="inherit">Community</Button>
-          </NavLink>
-          <NavLink style={applyNavStyle} to={ROUTES.CHAT} end>
-            <Button color="inherit">Helpline</Button>
-          </NavLink>
+          {width ? (
+            <>
+              <IconButton onClick={() => setIsOpen(true)}>
+                <MenuIcon />
+              </IconButton>
+              <Drawer
+                anchor="right"
+                open={isOpen}
+                onClose={handleClose}
+                PaperProps={{ sx: { backgroundColor: "#F1C043" } }}
+              >
+                <IconButton sx={{ mb: 2 }} disableRipple>
+                  <CloseIcon onClick={handleClose} />
+                </IconButton>
+
+                <Box
+                  display="flex"
+                  flexDirection="column"
+                  sx={{ backgroundColor: "inherit", width: "200px" }}
+                >
+                  <NavLink style={applyNavStyle} to={ROUTES.DEFAULT} end>
+                    <Button color="inherit" onClick={handleClose}>
+                      Home
+                    </Button>
+                  </NavLink>
+                  <NavLink style={applyNavStyle} to={ROUTES.DRUG_FORM} end>
+                    <Button color="inherit" onClick={handleClose}>
+                      Form
+                    </Button>
+                  </NavLink>
+                  <NavLink style={applyNavStyle} to={ROUTES.COMMUNITY} end>
+                    <Button color="inherit" onClick={handleClose}>
+                      Community
+                    </Button>
+                  </NavLink>
+                  <NavLink style={applyNavStyle} to={ROUTES.CHAT} end>
+                    <Button color="inherit" onClick={handleClose}>
+                      Helpline
+                    </Button>
+                  </NavLink>
+                </Box>
+              </Drawer>
+            </>
+          ) : (
+            <>
+              <NavLink style={applyNavStyle} to={ROUTES.DEFAULT} end>
+                <Button color="inherit">Home</Button>
+              </NavLink>
+              <NavLink style={applyNavStyle} to={ROUTES.DRUG_FORM} end>
+                <Button color="inherit">Form</Button>
+              </NavLink>
+              <NavLink style={applyNavStyle} to={ROUTES.COMMUNITY} end>
+                <Button color="inherit">Community</Button>
+              </NavLink>
+              <NavLink style={applyNavStyle} to={ROUTES.CHAT} end>
+                <Button color="inherit">Helpline</Button>
+              </NavLink>
+            </>
+          )}
         </Toolbar>
       </AppBar>
       <Outlet />
