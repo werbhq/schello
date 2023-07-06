61b97acac33eae4290ec28fae0260b9249444949
Navaneeth Venu <nauaneeth@gmail.com>
2023-07-06 20:33:38 +0530

    Update Timestamp Display Format
    User Readable Format optimised for Social Media (x units ago)

--- src/pages/Home/components/HomeCard.tsx
+++ src/pages/Home/components/HomeCard.tsx
@@ -57,6 +57,33 @@ export default function MediaCard(props: {
 
     const expand = props.expand ?? true;
 
+    const formatTimestamp = (timestamp: string) => {
+        const currentTime = new Date().getTime();
+        const timeDiff = currentTime - new Date(timestamp).getTime();
+
+        // Convert milliseconds to different time units
+        const seconds = Math.floor(timeDiff / 1000);
+        const minutes = Math.floor(seconds / 60);
+        const hours = Math.floor(minutes / 60);
+        const days = Math.floor(hours / 24);
+        const months = Math.floor(days / 30);
+        const years = Math.floor(months / 12);
+
+        if (seconds < 60) {
+            return `${seconds} second${seconds !== 1 ? 's' : ''} ago`;
+        } else if (minutes < 60) {
+            return `${minutes} minute${minutes !== 1 ? 's' : ''} ago`;
+        } else if (hours < 24) {
+            return `${hours} hour${hours !== 1 ? 's' : ''} ago`;
+        } else if (days < 30) {
+            return `${days} day${days !== 1 ? 's' : ''} ago`;
+        } else if (months < 12) {
+            return `${months} month${months !== 1 ? 's' : ''} ago`;
+        } else {
+            return `${years} year${years !== 1 ? 's' : ''} ago`;
+        }
+    };
+
     // let description: any = stringToHtml('description' in props ? props.data.description ?? '' : '');
 
     return (
@@ -173,7 +200,7 @@ export default function MediaCard(props: {
 
                     <SDSChip
                         sx={{ flexShrink: 0 }}
-                        label={expand ? (props.data.fromExcise ? 'Excise' : 'Community') : ''}
+                        label={props.data.fromExcise ? 'Excise' : 'Community'}
                         color={props.data.fromExcise ? 'primary' : 'secondary'}
                         icon={props.data.fromExcise ? <Verified /> : <People />}
                     ></SDSChip>
@@ -201,7 +228,7 @@ export default function MediaCard(props: {
                             <AccessTimeFilledRounded />
                             <p style={{ fontSize: '0.8em', margin: '0', padding: '0px 5px' }}>
                                 {props.data.type === 'MEDIA'
-                                    ? new Date(props.data.timestamp).toDateString()
+                                    ? formatTimestamp(props.data.timestamp)
                                     : displayTimeDateString}
                             </p>
                         </Stack>
